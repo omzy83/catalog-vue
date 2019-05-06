920a01079a9067fd218797f223aaf19b295860f9
Omar Ahmed <omzy83@outlook.com>
2019-05-06 15:11:18 +0100

    adding login

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,29 @@
         <b-navbar-nav>
           <b-nav-item to="/">Home</b-nav-item>
           <b-nav-item to="/products">Products</b-nav-item>
-          <b-nav-item href="#" @click.prevent="login">Login</b-nav-item>
-          <b-nav-item href="#" @click.prevent="logout">Logout</b-nav-item>
+          <b-nav-item to="/login" v-if="!isLoggedIn">Login</b-nav-item>
+          <b-nav-item href="#" @click.prevent="logout" v-else>Logout</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
     <router-view />
   </div>
 </template>
+
+<script>
+  export default {
+    computed: {
+      isLoggedIn: function() {
+        return this.$store.getters.isLoggedIn
+      }
+    },
+    methods: {
+      logout: function () {
+        this.$store.dispatch('logout')
+        .then(() => {
+          this.$router.push('/login')
+        })
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,18 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
 
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token')
+
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
 
 new Vue({
   router,
